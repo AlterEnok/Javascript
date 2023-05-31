83cf157e943114aacdc3eacf09872665c25a8b8d
AlterEnok <ed.kestan@gmail.com>
2023-05-31 17:17:42 +0300

    1

---
+++ Arrays/task2/index.js
@@ -0,0 +1,19 @@
+const numbers = [11, 22, 33, 55, 66];
+
+function sum(numbers) {
+  let sumOfNumbers = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sumOfNumbers += numbers[i];
+  }
+  return sumOfNumbers;
+}
+const sumOfElements = sum(numbers);
+console.log('Result: ' + sumOfElements);
+
+// const numbers = [11, 22, 33, 55, 66];
+
+// const arr = numbers;
+
+// arr[2] = 17;
+// console.log(numbers);
