f2236c6efa3069901e62f44e7de522dc722c55c3
AlterEnok <ed.kestan@gmail.com>
2023-06-20 18:48:27 +0300

    increase

---
+++ Arrays-methods/Increase/index.js
@@ -0,0 +1,23 @@
+// const increaseEvenE1 = (arr, delta) => {
+//   if (!Array.isArray(arr)) {
+//     return null;
+//   }
+
+//   return arr.map((num) => {
+//     if (num % 2 === 0) {
+//       return num + delta;
+//     }
+//     return num;
+//   });
+// };
+
+// console.log(increaseEvenE1([2, 5, 6, 8, 11, 9, 4], 20));
+
+// compact code
+const increaseEvenl = (arr, delta) => {
+  return Array.isArray(arr)
+    ? arr.map((num) => (num % 2 === 0 ? num + delta : num))
+    : null;
+};
+
+console.log(increaseEvenl([2, 5, 6, 8, 11, 9, 4], 20));
