c35fa6e4967589c7496e383a51238d5ac0848aa5
AlterEnok <ed.kestan@gmail.com>
2023-06-21 21:13:33 +0300

    update-reverse

--- Arrays-methods/Reverse-arr/index.js
+++ Arrays-methods/Reverse-arr/index.js
@@ -1,10 +1,18 @@
-const reverseArray = (arr) => {
-  if (!Array.isArray(arr)) {
-    return null;
-  }
+// const reverseArray = (arr) => {
+//   if (!Array.isArray(arr)) {
+//     return null;
+//   }
 
-  return arr.slice().reverse();
-};
+//   return arr.slice().reverse();
+// };
+
+// console.log(reverseArray([1, 2, 3, 4, 5]));
+// console.log(reverseArray([17, 18, 19, 20]));
+
+// compact code
+
+const reverseArray = (arr) =>
+  Array.isArray(arr) ? arr.slice().reverse() : null;
 
 console.log(reverseArray([1, 2, 3, 4, 5]));
 console.log(reverseArray([17, 18, 19, 20]));
