2cbe21ce89327db210edfe5b8b7b33159e2ced71
AlterEnok <ed.kestan@gmail.com>
2023-07-17 21:46:59 +0300

    task1

---
+++ Lexical-environment/task1/index.js
@@ -0,0 +1,10 @@
+let message = 'Just learn it!';
+
+export const sendMessage = (name) => {
+  console.log(`${name}, ${message} Your Gromcode`);
+  return `${name}, ${message} Your Gromcode`;
+};
+
+export const setMessage = (newMessage) => {
+  message = newMessage;
+};
