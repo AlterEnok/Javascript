1b281104981ef5759707e2b55c8a789e9d141a38
AlterEnok <ed.kestan@gmail.com>
2023-07-30 21:29:03 +0300

    task7

---
+++ Context/task7/index.js
@@ -0,0 +1,14 @@
+export const user = {
+  firstName: '',
+  lastName: '',
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+
+  setFullName(fullName) {
+    const [firstName, lastName] = fullName.split(' ');
+    this.firstName = firstName;
+    this.lastName = lastName;
+  },
+};
