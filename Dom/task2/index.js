302548ccb1f9ec7cb4d02d9e7b31b4942e34411b
AlterEnok <ed.kestan@gmail.com>
2023-08-11 00:09:53 +0300

    task2

---
+++ Dom/task2/index.js
@@ -0,0 +1,11 @@
+function getTitileElement() {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
+}
+
+function getInputElement() {
+  const inputElem = document.querySelector('input[type="text"]');
+  console.dir(inputElem);
+  return inputElem;
+}
