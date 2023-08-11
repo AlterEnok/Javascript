fef58839fa7d8a2a333500fa321fd292fc5a9c11
AlterEnok <ed.kestan@gmail.com>
2023-08-11 17:34:16 +0300

    task5

---
+++ Dom/task5/index.js
@@ -0,0 +1,16 @@
+function setButton(buttonText) {
+  const bodyElem = document.querySelector('body');
+  if (bodyElem) {
+    bodyElem.innerHTML = `<button>${buttonText}</button>`;
+  }
+}
+
+setButton('Click me!');
+
+// Second code with textContent
+// export function setButton(buttonText) {
+//   const bodyElem = document.querySelector('body');
+//   if (bodyElem) {
+//     bodyElem.textContent = `button${buttonText}/button`;
+//   }
+// }
