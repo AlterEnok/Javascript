cdae9be6a8e91a6a97d6ace9b9699210f701527a
AlterEnok <ed.kestan@gmail.com>
2023-08-11 15:15:28 +0300

    task4

---
+++ Dom/task4/index.js
@@ -0,0 +1,19 @@
+export function getTitle() {
+  const titleElem = document.querySelector('.title');
+  return titleElem.textContent;
+}
+
+export function getDescription() {
+  const aboutElem = document.querySelector('.about');
+  return aboutElem.innerText;
+}
+
+export function getPlans() {
+  const plansElem = document.querySelector('.plans');
+  return plansElem.innerHTML;
+}
+
+export function getGoal() {
+  const goalElem = document.querySelector('goal');
+  return goalElem.outerHTML;
+}
