3a1ddd83bea48df6c6ecef6b08f386a2c025df76
AlterEnok <ed.kestan@gmail.com>
2023-08-12 00:24:50 +0300

    task8

--- Dom/task8/index.js
+++ Dom/task8/index.js
@@ -1,12 +1,24 @@
 export function finishList() {
-  const categoriesList = document.querySelector('.categories');
+  const list = document.querySelector('.list');
 
   for (let i = 1; i <= 8; i++) {
     const listItem = document.createElement('li');
-    listItem.classList.add('tool');
-    listItem.textContent = `Item ${i}`;
+    listItem.textContent = i;
 
-    categoriesList.append(listItem);
+    list.append(listItem);
+
+    if (i === 1) {
+      const firstItem = document.createElement('li');
+      firstItem.textContent = '1 - before';
+      list.prepend(firstItem);
+    }
+
+    if (i === 6) {
+      const newItem = document.createElement('li');
+      newItem.textContent = '6 - after special';
+      const specialItem = document.querySelector('.special');
+      specialItem.after(newItem);
+    }
   }
 }
 
