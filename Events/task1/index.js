f012c1f4090b11caf7a6077c300b58f0d4bd63b0
AlterEnok <ed.kestan@gmail.com>
2023-08-17 18:40:30 +0300

    task

--- Events/task1/index.js
+++ Events/task1/index.js
@@ -20,15 +20,69 @@
 // spanElem.addEventListener('click', logGreySpan);
 // spanElem.addEventListener('click', logGreenSpan);
 
+// document.addEventListener('DOMContentLoaded', () => {
+//   const divElem = document.querySelector('.rect_div');
+//   const pElem = document.querySelector('.rect_p');
+//   const spanElem = document.querySelector('.rect_span');
+//   const eventsListElem = document.querySelector('.events-list');
+
+//   const logTarget = (text, color) => {
+//     const eventsListElem = document.querySelector('.events-list');
+//     eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
+//   };
+
+//   const clearButton = document.querySelector('.clear-btn');
+//   clearButton.addEventListener('click', () => {
+//     eventsListElem.innerHTML = '';
+//   });
+
+//   const attachButton = document.querySelector('.attach-handlers-btn');
+//   attachButton.addEventListener('click', () => {
+//     divElem.addEventListener(
+//       'click',
+//       logTarget.bind(null, 'div', 'grey'),
+//       true
+//     );
+//     divElem.addEventListener('click', logTarget.bind(null, 'div', 'green'));
+
+//     pElem.addEventListener('click', logTarget.bind(null, 'p', 'grey'), true);
+//     pElem.addEventListener('click', logTarget.bind(null, 'p', 'green'));
+
+//     spanElem.addEventListener('click', logTarget.bind(null, 'span', 'grey'));
+//     spanElem.addEventListener('click', logTarget.bind(null, 'span', 'green'));
+//   });
+
+//   const removeButton = document.querySelector('.remove-handlers-btn');
+//   removeButton.addEventListener('click', () => {
+//     divElem.removeEventListener(
+//       'click',
+//       logTarget.bind(null, 'div', 'grey'),
+//       true
+//     );
+//     divElem.removeEventListener('click', logTarget.bind(null, 'div', 'green'));
+
+//     pElem.removeEventListener('click', logTarget.bind(null, 'p', 'grey'), true);
+//     pElem.removeEventListener('click', logTarget.bind(null, 'p', 'green'));
+
+//     spanElem.removeEventListener('click', logTarget.bind(null, 'span', 'grey'));
+//     spanElem.removeEventListener(
+//       'click',
+//       logTarget.bind(null, 'span', 'green')
+//     );
+//   });
+// });
 document.addEventListener('DOMContentLoaded', () => {
   const divElem = document.querySelector('.rect_div');
   const pElem = document.querySelector('.rect_p');
   const spanElem = document.querySelector('.rect_span');
   const eventsListElem = document.querySelector('.events-list');
 
-  const logTarget = (text, color) => {
-    const eventsListElem = document.querySelector('.events-list');
-    eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
+  const logTarget = (target, color) => {
+    const span = document.createElement('span');
+    span.style.color = color;
+    span.style.marginLeft = '8px';
+    span.textContent = target;
+    eventsListElem.appendChild(span);
   };
 
   const clearButton = document.querySelector('.clear-btn');
