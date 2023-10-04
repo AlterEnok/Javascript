d2bd0bb24041af025c1ece419ca6f859d1d39445
AlterEnok <ed.kestan@gmail.com>
2023-10-04 22:33:43 +0300

    update bugs

--- Working-dates/task1/index.js
+++ Working-dates/task1/index.js
@@ -1,11 +1,11 @@
 const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
-const getDayOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
   const newDate = new Date(date);
   newDate.setDate(newDate.getDate() + days);
 
   return weekDays[newDate.getDay()];
 };
-const result = getDayOfWeek(new Date(2022, 2, 1), 4);
+const result = dayOfWeek(new Date(2022, 2, 1), 4);
 
 console.log(result);
