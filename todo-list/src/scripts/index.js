94a6455b889a82cc03934af013ca3f07a4cf14ed
AlterEnok <108426805+AlterEnok@users.noreply.github.com>
2023-10-14 21:36:14 +0300

    Add files via upload

---
+++ todo-list/src/scripts/index.js
@@ -0,0 +1,17 @@
+
+import { initTodoListHandlers} from './todoList';
+import { renderTasks } from './renderer';
+document.addEventListener('DOMContentLoaded', () =>{
+  renderTasks();
+  initTodoListHandlers();
+})
+
+const onStorageChange = e =>{
+    if(e.key === 'tasksList'){
+        renderTasks();
+    }
+
+
+};
+
+window.addEventListener('storage', onStorageChange);
